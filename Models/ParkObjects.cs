4b7ec658aa8bd16fdc81d031de65ce1ad172f46b
Vera <verakluetz@gmail.com>
2023-11-19 01:18:12 -0500

    Models
    Added the models folder with the model how it is shown on our project page

---
+++ Models/ParkObjects.cs
@@ -0,0 +1,41 @@
+﻿namespace DataGov_API_Intro_6.Models
+{
+    public class Regions
+    {
+        public string region_name { get; set; }
+        public List<Counties> counties { get; set; }
+    }
+
+    public class Counties
+    {
+        public string county_name { get; set; }
+        public Regions regions { get; set; }
+        public Vehicles vehicles { get; set; }
+    }
+
+    public class Vehicles
+    {
+        public string vin { get; set; }
+        public string make_name { get; set; }
+        public string model_name { get; set; }
+        public int range { get; set; }
+        public Counties counties { get; set; }
+        public Makes makes { get; set; }
+        public Models models { get; set; }    
+    }
+
+    public class Makes
+    {
+        public string make_name { get; set; }
+        public string model_name { get; set; }
+        public List<Vehicles> vehicles { get; set; }
+        public List<Models> models { get; set; }
+    }
+
+    public class Models
+    {
+        public string model_name { get; set; }
+        public Makes makes { get; set;}
+        public Vehicles vehicles { get; set; }
+    }
+}
